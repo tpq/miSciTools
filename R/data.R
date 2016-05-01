08bb8bb8ef5f467f5602f3fa93660ba96c9396f3
Thom Quinn <contacttomquinn@gmail.com>
2016-05-01 19:47:59 -0400

    Added 'Linc2GO' data documentation

--- R/data.R
+++ R/data.R
@@ -1,6 +1,25 @@
 #' Linc2GO GeneSetCollection
 #'
-#' These data [placeholder].
+#' These data expose the Linc2GO database as a \code{GeneSetCollection} object
+#'  to use in conjunction with the \code{Category} and \code{GOstats}
+#'  packages for gene set enrichment analysis. The gene ontology terms
+#'  in this database stem from an application of the competing endogenous
+#'  RNA (ceRNA) hypothesis which speculates that lncRNAs regulate expression,
+#'  at least in part, by competing with mRNAs for microRNA binding sites.
+#'  Since microRNAs bind mRNA transcriptS to silence their expression,
+#'  a microRNA-lncRNA complex could free up that mRNA for expression.
+#'  Therefore, the curators integrate microRNA-mRNA and microRNA-lncRNA
+#'  binding patterns to predict potential lncRNA-mRNA collaborations.
+#'  Based on the gene ontology of the mRNA transcript pairs, the curators can
+#'  make an educated guess about the gene ontology of the lncRNA.
+#'
+#' This database uses the lincRNA ID archived by the Human lincRNA Catalog.
+#'  Conveniently, these represent the same terms used by UCSC in the
+#'  \code{TxDb.Hsapiens.UCSC.hg19.lincRNAsTranscripts} \code{TxDb} object.
+#'
+#' @format A \code{GeneSetCollection} object from the \code{GSEABase} package.
+#'
+#' @source \url{http://www.bioinfo.tsinghua.edu.cn/~liuke/Linc2GO/}
 #'
 #' @example
 #' \dontrun{
@@ -21,4 +40,4 @@
 #' Over <- GOstats::hyperGTest(params)
 #' head(summary(Over))
 #' }
-NULL
+"linc2go"
